b2c29aaeb6248e083e5efc6c3eb8279e2087d8c5
Ovrcst <48123080+Ovrcst@users.noreply.github.com>
2022-05-19 20:24:52 +0800

    Add files via upload

---
+++ auto.py
@@ -0,0 +1,53 @@
+import os
+import shutil
+
+path = r'path\to\windows\file'
+images = ['png','jpeg','gif','jpg','jpe','jfif']
+videos = ['mov','mp4','wmv','avi','flv','mkv','webm']
+audio = ['m4a','mp3','flac','wav','wma','aac']
+documents = ['docx','doc','pdf','odt','rtf','txt','pptx','ppt','rtf']
+
+while(True):
+    for item in os.listdir(path):
+        complete_path = os.path.join(path,item)
+        if(os.path.isdir(complete_path)):
+            continue
+        try:
+            if(item.lower().endswith(tuple(images))):
+                to_create = 'Images'
+                os.rename(f'{path}\{item}',f'{path}\{to_create}\{item}')\
+
+            elif(item.lower().endswith(tuple(audio))):
+                to_create = 'Audio'
+                shutil.move(f'{path}\{item}',f'{path}\{to_create}\{item}')
+
+            elif(item.lower().endswith(tuple(documents))):
+                to_create = 'Documents'
+                shutil.move(f'{path}\{item}',f'{path}\{to_create}\{item}')
+
+            elif(item.lower().endswith(tuple(videos))):
+                to_create = 'Videos'
+                shutil.move(f'{path}\{item}',f'{path}\{to_create}\{item}')
+            else:
+                to_create = 'Others'
+                shutil.move(f'{path}\{item}',f'{path}\{to_create}\{item}')
+
+
+        except FileNotFoundError:
+            os.mkdir(f'{path}\{to_create}')
+
+            
+        except FileExistsError:
+            if(os.path.exists(f'{path}\{to_create}\{item}') and not os.path.exists(f'{path}\{to_create}\(Copy){item}')):
+                shutil.move(f'{path}\{item}', f'{path}\{to_create}\(Copy){item}')
+                continue
+            else:
+                count = 1
+                while(os.path.exists(f'{path}\{to_create}\(Copy{count}){item}')):
+                    count += 1
+                shutil.move(f'{path}\{item}', f'{path}\{to_create}\(Copy{count}){item}')
+
+
+        except PermissionError as e:
+            print(e)
+            continue
